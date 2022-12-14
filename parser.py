745157c6214790bf47f03e114b0c651a596d8b2d
MarcosRecio <marcosmanuelrecioperez@usal.es>
2022-12-14 21:54:39 +0100

    Update parser

--- parser.py
+++ parser.py
@@ -2,17 +2,21 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import collections
+import functools
+import pyparsing
+
 """
-    Copyright 2018 Google LLC
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-        https://www.apache.org/licenses/LICENSE-2.0
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
+Copyright 2022 Marcos Recio
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    https://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
 
 """
@@ -42,21 +46,10 @@ relation f(x, y) is parsed as a ternary op, with op-type "R2", and arguments
 For all "all x . k" is parsed as a binary op, with op-type "A" and arguments
 [x, k]. Similarly for "exists x . k".
 """
-import collections
-import functools
-
-import pyparsing
-
-
-
-
-
-
 
 class Language(object):
-  """Contains information on a language (e.g., propn. or first-order logic).
-  This is used by `TreeNet` to learn how to interpret the symbols, and how many
-  "variable" embeddings to create
+  """
+  Contains information on a language (e.g., propn. or first-order logic).
   """
 
   def __init__(self, arities, predicates, constants, variables):
